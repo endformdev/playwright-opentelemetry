4dc2a8e8f9365e158eb94adcd464b77d5c48fd10
Oliver Stenbom <oliver@stenbom.eu>
2025-11-05 12:47:25 +0100

    Add base reporter

---
+++ README.md
@@ -0,0 +1,5 @@
+# Playwright Opentelemetry Reporter
+
+We're building a reporter for Playwright that can export traces and spans in opentelemetry format.
+
+We're just getting started, this project is in early development. Reach out if you're interested in learning more!
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export function fn() {
-	return "Hello, tsdown!";
+import type {
+	FullResult,
+	Reporter,
+	TestCase,
+	TestResult,
+} from "@playwright/test/reporter";
+
+export default class PlaywrightOpentelemetryReporter implements Reporter {
+	onEnd(result: FullResult) {
+		console.log(result);
+	}
+
+	onTestBegin(test: TestCase) {
+		console.log(test.title);
+	}
+
+	onTestEnd(test: TestCase, result: TestResult) {
+		console.log(test.title, result.status);
+	}
 }
