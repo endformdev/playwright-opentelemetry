b189a29fbdc59125a44022f3032ad66ce7410435
Oliver Stenbom <oliver@stenbom.eu>
2025-12-22 16:37:22 +0000

    No ts sourcemaps on build

--- trace-reporter/tsdown.config.ts
+++ trace-reporter/tsdown.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-	dts: true,
+	dts: {
+		sourcemap: false,
+	},
 	entry: {
 		reporter: "./src/reporter",
 		fixture: "./src/fixture",
